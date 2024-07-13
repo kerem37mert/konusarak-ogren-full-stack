139b9997a9c7c53e13f0546f92b97fcb54cea182
Kerem Mert <kerem37mert@yahoo.com>
2024-07-13 11:19:34 +0300

    fav context updated

--- frontend/src/contexts/FavContext.jsx
+++ frontend/src/contexts/FavContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { json } from "react-router-dom";
 
 const FavContext = createContext();
@@ -6,6 +6,11 @@ const FavContext = createContext();
 const FavProvider = ({ children }) => {
 
     const [favs, setFavs] = useState([]);
+
+    useEffect(() => {
+        if(localStorage.getItem("myFavChars"))
+            setFavs(JSON.parse(localStorage.getItem("myFavChars")));
+    }, []);
 
     return(
         <FavContext.Provider value={{ favs, setFavs }}>
